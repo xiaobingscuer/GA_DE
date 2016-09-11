1e2b78da422536e856f3bde0dc05781f84bca95d
xiaobingscuer <2536946512@qq.com>
2016-09-11 16:39:34 +0800

    two

--- DEOptimalOne.java
+++ DEOptimalOne.java
@@ -2,29 +2,24 @@
 import java.util.Random;
 /////////
 // author:xiaobingscuer
-// topic:GA algorithm one,关于遗传算法（一）
-// 这是用 遗传算法 求解一维 最优问题: min f(x),x的范围(-5,5)
+// topic:DE algorithm one,关于差分进化算法（一）
+// 这是用 差分进化算法 求解一维 最优问题: min f(x),x的范围(-5,5)
 // 此程序中，f(x)=(x-2)^2,最优解为 2.000
-// 遗传算法采用实数编码，轮盘赌选择，变异为加上小的随机数
+// 差分进化算法采用实数编码，轮盘赌选择，繁殖方式为newPopulation[i]=population[r1]+(population[r2]-population[r3])
 // 由于f(x)>0,故个体适应度评估函数为 1/f(x)
-// 遗传算法的其他观点：
-// 		进化出策略，比如工程上的方案策略，或者机器人的行动策略(可参见书《复杂》）
-// 		求解动态规划
 /////////
-public class GAOptimalOne {
+public class DEOptimalOne {
 	// 种群规模
 	private final static  int POPULATION_SCALE=1000;
 	// 进化代数
 	private final int EVALUTION_NUM=100;
-	// 交叉概率
-	//private final float CROSS_PROBABLITY=0.5F;
-	// 变异概率
-	private final float MUTATE_PROBABLITY=0.1F;
+	// 繁殖概率
+	private final float BREED_PROBABLITY=0.9F;
 	// x域
 	float max=5;
 	float min=-5;
 		
-	public GAOptimalOne() {
+	public DEOptimalOne() {
 		super();
 	}
 	
@@ -72,29 +67,30 @@ public class GAOptimalOne {
 		}
 		return newPopulation;
 	}
-	// 交叉
-	public float[] cross(float[] population){			// 因采用实数编码，并且只是一维问题，故没进行交叉操作
+	// 繁殖,将遗传算法的交叉操作和变异操作合并为繁殖操作
+	public float[] breed(float[] population){
 		float[] newPopulation=new float[population.length];
-		Random rand=new Random();						// 差分进化算法DE中就采用的实数编码，将交叉和变异合并为繁殖
-		// 实数编码无交叉
-		newPopulation=population;
-		return newPopulation;
-	}
-	// 变异
-	public float[] mutate(float[] population){
-		float[] newPopulation;
 		Random rand=new Random();
+		int r1,r2,r3;
 		for(int i=0;i<population.length;i++){
-			if(rand.nextFloat()<MUTATE_PROBABLITY){
-				population[i]+=0.1*rand.nextFloat();	// 随机变异，即个体加上一个小的随机数
-				if(population[i]>5){					// 判断个体是否在x域里
-					population[i]=5;
-				}if(population[i]<-5){
-					population[i]=-5;
+			if(rand.nextFloat()<BREED_PROBABLITY){		// 判断是否需要繁殖
+				r1=rand.nextInt(population.length);
+				do{
+					r2=rand.nextInt(population.length);
+				}while(r2==r1);
+				do{
+					r3=rand.nextInt(population.length);
+				}while(r3==r2||r3==r1);
+				newPopulation[i]=population[r1]+(population[r2]-population[r3]); // 繁殖方式
+				if(newPopulation[i]>5){					// 判断个体是否在x域里
+					newPopulation[i]=5;
+				}if(newPopulation[i]<-5){
+					newPopulation[i]=-5;
 				}
+			}else{
+				newPopulation[i]=population[i];
 			}
 		}
-		newPopulation=population;
 		return newPopulation;
 	}
 	// 进化
@@ -105,8 +101,7 @@ public class GAOptimalOne {
 		for(int i=0;i<EVALUTION_NUM;i++){
 			fitList=fitnessList(newPopulation);			// 适应度评估
 			newPopulation=choice(newPopulation,fitList);// 个体选择
-			newPopulation=cross(newPopulation);			// 个体间交叉
-			newPopulation=mutate(newPopulation);		// 个体变异
+			newPopulation=breed(newPopulation);			// 繁殖
 		}
 		return newPopulation;
 	}
@@ -127,12 +122,13 @@ public class GAOptimalOne {
 	}
 	
 	public static void main(String[] args) {
-
+		
 		long startTime=System.currentTimeMillis();			// 开始时间
-		GAOptimalOne gaOptimal=new GAOptimalOne();			// 定义对象
-		float[] bestPer=gaOptimal.bestPerson(gaOptimal.evlution(gaOptimal.generatePoupulation(POPULATION_SCALE))); // 方法调用 // 进化// 选出最优个体	
-		long endTime=System.currentTimeMillis();			// 结束时间		
+		DEOptimalOne deOptimal=new DEOptimalOne();			// 定义对象
+		float[] bestPer=deOptimal.bestPerson(deOptimal.evlution(deOptimal.generatePoupulation(POPULATION_SCALE))); // 方法调用
+		long endTime=System.currentTimeMillis();			// 结束时间	
 		System.out.print("最优个体的适应度："+bestPer[0]+"\n"+"最优解："+bestPer[1]+"\n"+"用时(ms):"+(endTime-startTime));
+		
 	}
 
 }
